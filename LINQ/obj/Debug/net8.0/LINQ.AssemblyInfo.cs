12ce67a8fdfdc0410fec0148b0da3dfb1b23f092
Serhatuzn <seerhatuzun@gmail.com>
2024-11-18 01:36:34 +0300

    Patikafy Music Project

--- LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3a0e92e2095e99ef07ed64a842fda638d88fa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d38c477015757668dcb33d7c71692896daab9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
