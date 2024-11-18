704a4a51c50ed67daf00fbb18f2cc2c20fccae0a
Serhatuzn <seerhatuzun@gmail.com>
2024-11-18 16:32:37 +0300

    Patikafy Music Platform Project

--- Patikafy/obj/Debug/net8.0/Patikafy.AssemblyInfo.cs
+++ Patikafy/obj/Debug/net8.0/Patikafy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patikafy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ce67a8fdfdc0410fec0148b0da3dfb1b23f092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2453f9f84b74657fe4e2f42eaf1f9b3e9d39d383")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patikafy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patikafy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
